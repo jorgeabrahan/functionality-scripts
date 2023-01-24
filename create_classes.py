1db508b693ec64f321cc1c670d45eb4e3cd01c4d
Jorge <jorge24abrahan@gmail.com>
2023-01-23 20:15:34 -0600

    Create script to add university folders

---
+++ create_classes.py
@@ -0,0 +1,23 @@
+import os
+
+def create_class_folders(class_name, path):
+    try:
+        class_dir_path = os.path.join(path, class_name)
+        os.mkdir(class_dir_path)
+        for i in range(1, 11):
+            week_dir_path = os.path.join(class_dir_path, 'W{i}'.format(i=i)) 
+            os.mkdir(week_dir_path)
+            os.mkdir(os.path.join(week_dir_path, 'resources'))
+            os.mkdir(os.path.join(week_dir_path, 'activities'))
+    except OSError as error:
+        print(error)
+
+
+def create_classes():
+    path = input('Enter the path in which you want to create the folders: ')
+    amount = input('Enter the amount of classes you want to create: ')
+
+    for c in range(int(amount)):
+        class_name = input('Write the class name: ').lower().rstrip().replace(' ', '_')
+        create_class_folders(class_name, path)
+
