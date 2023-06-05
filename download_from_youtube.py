35a1267801e20df3d66546207a90f5ee19052f3f
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-05 09:56:15 -0600

    Update download from youtube file functionallity to work with the newly implemented changes in the youtube API

--- download_from_youtube.py
+++ download_from_youtube.py
@@ -1,18 +1,21 @@
-from pytube import YouTube, Playlist
+import yt_dlp
 import os
+import re
 
+# This file downloads videos or playlists from youtube as audio or video depending on selection and stores
+# the files in a folder called: Audio or Video depending on the selected option
 
-def ask_for_option(toAskFor, options):
+def ask_for_option(to_ask_for, options):
     repeat = True
     while repeat:
-        print(toAskFor)
+        print(to_ask_for)
         for index, option in enumerate(options, start=1):
             print(f"{index}. {option}")
         try:
             selected = int(input("Select the option writing the number: "))
             repeat = selected > len(options) or selected < 1
             if repeat:
-                print("Select a valid option")
+                print("Select a valid option!")
             else:
                 print("")
                 return [selected, options[selected - 1]]
@@ -20,90 +23,53 @@ def ask_for_option(toAskFor, options):
             print("The option selected should be a number")
 
 
-def get_file(to_download, format):
-    if format == 1:
-        return to_download.streams.filter(only_audio=True).first()
-    return to_download.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+def rename_files(option_str):
+    # rename all files once they've been downloaded
+    for filename in os.listdir(option_str):
+        if '_' not in filename: # if the file hasnt been renamed before
+            filepath = os.path.join(option_str, filename)
+            if os.path.isfile(filepath): # if the file exists
+                name, ext = os.path.splitext(filename) # get file name and extension
+                name_parts = name.rsplit(' ', 1) # split name in case it has an auto generated id
+                if len(name_parts) == 2:
+                    name, video_id = name_parts
+                    video_id = f"_{video_id}" # if theres a video id get it
+                else:
+                    video_id = "" # otherwise video id will be an empty string
+                name = name.strip() # trim or remove empty or white spaces at the end and beginning of name
+                name = re.sub(r'[-,]', '', name)  # remove all special characters (- and ,)
+                name = re.sub(r'\s+', '_', name)  # replace all whitespace characters with '_'
+                name = name.lower()
+                new_filename = f"{name}{video_id}{ext}"
+                new_filepath = os.path.join(option_str, new_filename)
+                os.rename(filepath, new_filepath) # rename the file
 
-
-def get_files(to_download, format, type):
-    files = []
-    if type == 1:
-        files.append(get_file(to_download, format))
+def get_ydl_opts(option_num, option_str):
+    if option_num == 1:
+        download_format = 'bestaudio[ext=mp3]/bestaudio'
+        postprocessors = [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
+        }] # after downloading the audio file it makes sure that it has the preferred codec and quality
     else:
-        for video in to_download.videos:
-            files.append(get_file(video, format))
-    return files
-
-
-def get_path(folder, title, type_num):
-    def_path = f".\\{folder}"
-    # if it is a playlist then create a folder with its name
-    if type_num == 2:
-        def_path += f"\\{title}"
-
-    print(f"Default path: {def_path} (where \".\\\" means current folder)")
-    return input("Enter the path to save the files (leave it blank for the default path): ") or def_path
-
-
-def set_file_extension(format, full_path):
-    if format == 1:
-        return f"{full_path}.mp3"
-    return f"{full_path}.mp4"
-
-
-def format_default_name(def_name):
-    return def_name.strip().replace("-", "").replace(" ", "_").replace("__", "_").lower()
-
-
-def get_path_with_new_name(path):
-    path_arr = path.split("\\")
-    default_name = format_default_name(str(path_arr[len(path_arr) - 1]))
-    path_without_file_name = path_arr[:-1]
-    while True:
-        print(f"Default file name: {default_name}")
-        file_name = input(
-            'Enter the file name (leave it blank for the default name): ') or default_name
-        path_without_file_name.append(file_name)
-        full_path = "\\".join(path_without_file_name)
-        if os.path.exists(full_path):
-            path_without_file_name.pop()
-            print(file_name + " already exists")
-        else:
-            return [full_path, file_name]
-
-
-def rename_file(out_file, format):
-    # path of the file to rename without the extension
-    path, ext = os.path.splitext(out_file)
-    full_path, file_name = get_path_with_new_name(path)
-    new_file = set_file_extension(format, full_path)
-    os.rename(out_file, new_file)
-    return file_name
+        download_format = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
+        postprocessors = []
+    return {
+        'format': download_format,
+         'outtmpl': f'{option_str}/%(title)s.%(ext)s',
+         'postprocessors': postprocessors
+    }
 
 
 def download_from_youtube():
-    try:
-        print("")
-        type_num, type_str = ask_for_option(
-            "What do you want to download?", ["Video", "Playlist"])
-        url = ""
-        while len(url) == 0:
-            url = input(f"{type_str} URL: ")
-        print("")
+    url = ""
+    while len(url) == 0:
+        url = input("Playlist or Video URL: ") # get url to download
+    option_num, option_str = ask_for_option("Format?", ["Audio", "Video"])
 
-        to_download = YouTube(url) if type_num == 1 else Playlist(url)
-
-        format, folder = ask_for_option(
-            "What format do you want?", ["Audio", "Video"])
-
-        files = get_files(to_download, format, type_num)
-        path = get_path(folder, to_download.title, type_num)
-        print("")
-        for file in files:
-            out_file = file.download(output_path=path)
-            file_name = rename_file(out_file, format)
-            print(file_name + " has been successfully downloaded\n")
-    except FileExistsError:
-        print(to_download.title + " already exists")
-
+    # download video or videos (in case of playlist)
+    with yt_dlp.YoutubeDL(get_ydl_opts(option_num, option_str)) as ydl:
+        ydl.download(url)
+    # rename all downloaded files
+    rename_files(option_str)
